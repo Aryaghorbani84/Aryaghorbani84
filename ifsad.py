76ba10257960ff115341785f5575e3ea65fe1700
Arya Ghorbani | آریا قربانی <140091625+Aryaghorbani84@users.noreply.github.com>
2023-08-18 21:34:47 +0330

    Create ifsad.py

---
+++ ifsad.py
@@ -0,0 +1,10 @@
+def stay_motivated(emotion):
+    if emotion.lower() in ["upset", "frustrated"]:
+        print("It's okay to feel upset or frustrated, but remember your goal!")
+        print("Take a deep breath and keep moving forward.")
+    else:
+        print("You're doing great! Keep up the good work and stay focused on your goal.")
+
+your_emotion = input("How are you feeling? ")
+
+stay_motivated(your_emotion)
